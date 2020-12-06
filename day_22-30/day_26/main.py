fd99c6122c82b41d75c36d1a0b17213d59498aa4
Haziq Roslan <haziq.roslan@digitas.com>
2020-12-07 01:01:12 +0800

    add try except clause to day 26

--- day_22-30/day_26/main.py
+++ day_22-30/day_26/main.py
@@ -4,7 +4,14 @@ data = pd.read_csv('./day_22-30/day_26/nato_phonetic_alphabet.csv')
 
 nato_conversion = {row.letter:row.code for (idx,row) in data.iterrows()}
 
-word_to_convert = input("What would you like to convert ?: ").upper()
+def generate_phonetic():
+    word_to_convert = input("What would you like to convert ?: ").upper()
+    try:
+        codeword = [nato_conversion[letter] for letter in word_to_convert]
+    except KeyError:
+        print ("Please, only letters")
+        generate_phonetic()
+    else:
+        print (codeword)
 
-codeword = [nato_conversion[letter] for letter in word_to_convert]
-print (codeword)
+generate_phonetic()
